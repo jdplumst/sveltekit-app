1ec8a99fca6abb43fd31eb7d200dcd43509d3503
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-02-01 14:13:16 -0500

    Set up db schema

---
+++ drizzle/0000_init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `session` (
+	`id` text PRIMARY KEY NOT NULL,
+	`session_token` text NOT NULL,
+	`user_id` integer NOT NULL,
+	`expires` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `todo` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`title` text NOT NULL,
+	`user_id` integer NOT NULL,
+	`status` text NOT NULL,
+	`priority` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`username` text NOT NULL,
+	`hashed_password` text NOT NULL
+);
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,5 +2,25 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+	username: text('username').notNull(),
+	hashedPassword: text('hashed_password').notNull()
+});
+
+export const session = sqliteTable('session', {
+	id: text('id').primaryKey(),
+	sessionToken: text('session_token').notNull(),
+	userId: integer('user_id')
+		.notNull()
+		.references(() => user.id),
+	expires: text('expires').notNull()
+});
+
+export const todo = sqliteTable('todo', {
+	id: integer('id').primaryKey(),
+	title: text('title').notNull(),
+	userId: integer('user_id')
+		.notNull()
+		.references(() => user.id),
+	status: text('status').notNull(),
+	priority: text('priority').notNull()
 });
