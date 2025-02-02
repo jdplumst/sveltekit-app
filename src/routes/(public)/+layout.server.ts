efee026d1b15b1860bb52b1f546266ada0336795
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-02-01 21:07:51 -0500

    Organize files to handle auth flow

--- src/routes/(public)/+layout.server.ts
+++ src/routes/(public)/+layout.server.ts
@@ -1,8 +1,8 @@
 import { db } from '$lib/server/db';
 import { sessions, users } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
-import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
 	const sessionToken = cookies.get('session');
@@ -16,7 +16,11 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
 	if (!session) return null;
 
 	const expireDate = new Date(session.expires);
-	if (expireDate < new Date()) return null;
+	if (expireDate < new Date()) {
+		cookies.delete('session', { path: '/' });
+		await db.delete(sessions).where(eq(sessions.sessionToken, sessionToken));
+		return null;
+	}
 
 	const user = await db
 		.select({ id: users.id, username: users.username })
